c16a2ac6b4197db70fc602acd30aa80145274978
JeeYulKim <kjy475@naver.com>
2023-02-17 19:29:30 +0900

    ✨ Answer 엔티티, 레포 , Auditable 인터페이스 추가

---
+++ server/cloneStackOverflow/src/main/java/com/preproject/cloneStackOverflow/answer/entity/Answer.java
@@ -0,0 +1,46 @@
+package com.preproject.cloneStackOverflow.answer.entity;
+
+import com.preproject.cloneStackOverflow.audit.Auditable;
+import com.preproject.cloneStackOverflow.member.entity.Member;
+import com.preproject.cloneStackOverflow.question.entity.Question;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Answer extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long answerId;
+
+    @Column(nullable = false, length = 2000)
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name ="MEMBER_ID")
+    private Member member;
+
+    @ManyToOne()
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    public Answer(String body) {
+        this.body = body;
+    }
+
+    public void addMember(Member member){
+        this.member = member;
+    }
+
+    public void addQuestion(Question question) {
+        this.question = question;
+    }
+
+
+
+}
