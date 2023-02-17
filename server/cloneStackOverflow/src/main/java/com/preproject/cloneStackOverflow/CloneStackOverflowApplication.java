9243407cb962592b205fa7968ab7f22025da2a13
SEB_BE_42_육경득 <115612484+DerekYook@users.noreply.github.com>
2023-02-17 11:03:39 +0900

    Add files via upload
    initial

---
+++ server/cloneStackOverflow/src/main/java/com/preproject/cloneStackOverflow/CloneStackOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.preproject.cloneStackOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneStackOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneStackOverflowApplication.class, args);
+	}
+
+}
