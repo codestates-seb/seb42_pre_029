1689df9818047930f681b67ba72cd618c0ec91f7
gwoongchoi <opo1220@gmail.com>
2023-02-21 15:50:35 +0900

    🔨 input 수정

--- client/src/components/InputBox.js
+++ client/src/components/InputBox.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import InputField from './InputField';
+import styled from 'styled-components';
 
 function InputBox() {
   const [email, setEmail] = useState('');
@@ -9,21 +10,37 @@ function InputBox() {
   const handlePasswordChange = event => setPassword(event.target.value);
 
   return (
-    <div>
+    <FormContainer>
       <InputField
+        type="text"
         label="Email"
         value={email}
         onChange={handleEmailChange}
         placeholder="your email?"
       />
       <InputField
+        type="password"
         label="Password"
         value={password}
         onChange={handlePasswordChange}
         placeholder="your password?"
       />
-    </div>
+    </FormContainer>
   );
 }
+
+const FormContainer = styled.div`
+  background: none;
+  border: 1px solid var(--line-003);
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  width: 400px;
+  height: 323px;
+  margin: 20px;
+`;
 
 export default InputBox;
