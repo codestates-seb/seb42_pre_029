2f1823a9f4d2b3d945c57640c3b855d9596cb017
Paksubeen <tnqls520@gmail.com>
2023-02-21 13:21:09 +0900

    🔨 MainLayout 컴포넌트 수정

--- client/src/components/MainLayout.js
+++ client/src/components/MainLayout.js
@@ -5,25 +5,34 @@ import Footer from './Footer';
 
 function MainLayout({ sideBar, children }) {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <ContentContainer>
         {sideBar && <SideBar />}
         <main>{children}</main>
       </ContentContainer>
       <Footer />
-    </>
+    </LayoutContainer>
   );
 }
+
+const LayoutContainer = styled.div`
+  &:first-child {
+    position: sticky;
+    top: 0;
+  }
+`;
 
 const ContentContainer = styled.div`
   display: flex;
   margin: 0 auto;
-  height: 100vh;
+  height: 100%;
   max-width: 1140px;
+  min-height: 100vh;
   & > main {
     flex-grow: 1;
     padding: 60px;
+    padding-right: 0px;
   }
 `;
 
