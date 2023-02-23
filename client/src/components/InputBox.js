9e5032091cebddc743fc43e920d79cf6a5c53c94
Paksubeen <tnqls520@gmail.com>
2023-02-23 13:24:01 +0900

    ♻️ Input + Label 컴포넌트 구현

--- client/src/components/InputBox.js
+++ client/src/components/InputBox.js
@@ -1,46 +1,22 @@
-import { useState } from 'react';
+import Label from './Label';
 import InputField from './InputField';
 import styled from 'styled-components';
 
-function InputBox() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = event => setEmail(event.target.value);
-  const handlePasswordChange = event => setPassword(event.target.value);
-
+function InputBox({ type, id, title, value, onChange }) {
   return (
     <FormContainer>
-      <InputField
-        type="text"
-        label="Email"
-        value={email}
-        onChange={handleEmailChange}
-        placeholder="your email?"
-      />
-      <InputField
-        type="password"
-        label="Password"
-        value={password}
-        onChange={handlePasswordChange}
-        placeholder="your password?"
-      />
+      <Label id={id}>{title}</Label>
+      <InputField type={type} id={id} value={value} onChange={onChange} />
     </FormContainer>
   );
 }
 
 const FormContainer = styled.div`
-  background: none;
-  border: 1px solid var(--line-003);
-  border-radius: 8px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  width: 400px;
-  height: 323px;
-  margin: 20px;
+  & > label {
+    margin-bottom: 8px;
+  }
 `;
 
 export default InputBox;
