bdd3790a3a6b321b6482158a32cdace5006dd3c9
Paksubeen <tnqls520@gmail.com>
2023-02-24 09:15:57 +0900

    ✨ 유효성 검사 기능 구현

--- client/src/pages/User/LoginForm.js
+++ client/src/pages/User/LoginForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Card from '../../components/Card';
 import InputBox from '../../components/InputBox';
@@ -7,25 +7,58 @@ import InputField from '../../components/InputField';
 import Button from '../../components/Button';
 
 function LoginForm() {
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-  });
+  const initialValues = { email: '', password: '' };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const onChange = e => {
+    const { id, value } = e.target;
+    setFormValues({ ...formValues, [id]: value });
+  };
 
   const onSubmit = e => {
     e.preventDefault();
+    setFormErrors(checkValid(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const checkValid = values => {
+    const errors = {};
+    const emailRegex = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z-.]+$/;
+    const passwordRegex = /^.*(?=^.{8,20}$)(?=.*\d)(?=.*[a-zA-Z]).*$/;
+
+    if (!values.email) {
+      errors.email = 'Email cannot be empty.';
+    } else if (!emailRegex.test(values.email)) {
+      errors.email = 'The email is not a valid email address.';
+    }
+    if (!values.password) {
+      errors.password = 'Password cannot be empty.';
+    } else if (!passwordRegex.test(values.password)) {
+      errors.password = 'Password must have valid email form.';
+    }
+    return errors;
   };
 
   return (
     <Card width="400px" height="320px">
       <InputForm onSubmit={onSubmit}>
         <InputBox
-          type="email"
+          type="text"
           id="email"
           title="Email"
-          value={form.email}
-          onChange={e => setForm({ ...form, email: e.target.value })}
+          value={formValues.email}
+          onChange={onChange}
         />
+        <ErrorMessage>{formErrors.email}</ErrorMessage>
         <PasswordContainer>
           <Label id="password">Password</Label>
           <a href="https://stackoverflow.com/">Forgot password?</a>
@@ -33,9 +66,10 @@ function LoginForm() {
         <InputField
           type="password"
           id="password"
-          value={form.password}
-          onChange={e => setForm({ ...form, password: e.target.value })}
+          value={formValues.email}
+          onChange={onChange}
         />
+        <ErrorMessage>{formErrors.password}</ErrorMessage>
         <Button
           text="Log in"
           width="320px"
@@ -56,11 +90,8 @@ const InputForm = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  & > div:first-child {
-    margin-bottom: 32px;
-  }
-  & > input {
-    margin-bottom: 32px;
+  & input {
+    margin-bottom: 4px;
   }
 `;
 
@@ -72,6 +103,13 @@ const PasswordContainer = styled.div`
   & > a {
     color: var(--main-001);
   }
+`;
+
+const ErrorMessage = styled.p`
+  margin-top: 8px;
+  margin-bottom: 16px;
+  color: var(--error);
+  font-size: var(--font-size-sm);
 `;
 
 export default LoginForm;
