155834caa8b639f555358563b194831ffb029126
gwoongchoi <opo1220@gmail.com>
2023-03-02 16:51:18 +0900

    mypage 변경

---
+++ src/pages/MyPage/EditModal.js
@@ -0,0 +1,212 @@
+import Button from '../../components/Button';
+import TextArea from '../../components/TextArea';
+import styled from 'styled-components';
+
+// const [editName, setEditName] = useState('');
+// const [editPassword, setEditPassword] = useState('');
+// const [editMail, setEditMail] = useState('');
+// const [editPhone, setEditPhone] = useState('');
+// const [editBirth, setEditBirth] = useState('');
+
+const EditModal = ({ open, onClose }) => {
+  if (!open) return null;
+  const handlePhoneNumChange = e => {
+    const rawPhoneNum = e.target.value;
+    const formattedPhoneNum = rawPhoneNum.replace(
+      /(\d{3})(\d{4})(\d{4})/,
+      '$1-$2-$3',
+    );
+    e.target.value = formattedPhoneNum;
+  };
+  const handleBirthDateChange = e => {
+    const rawBirthDate = e.target.value;
+    const formattedBirthDate = rawBirthDate.replace(
+      /(\d{4})(\d{2})(\d{2})/,
+      '$1.$2.$3',
+    );
+    e.target.value = formattedBirthDate;
+  };
+
+  // const editNameHandler = e => {
+  //   e.preventDefault();
+  //   setEditName(e.target.value);
+  // };
+  // const editPasswordHandler = e => {
+  //   e.preventDefault();
+  //   setEditPassword(e.target.value);
+  // };
+  // const editMailHandler = e => {
+  //   e.preventDefault();
+  //   setEditMail(e.target.value);
+  // };
+  // const editPhoneHandler = e => {
+  //   e.preventDefault();
+  //   setEditPhone(e.target.value);
+  // };
+  // const editBirthHandler = e => {
+  //   e.preventDefault();
+  //   setEditBirth(e.target.value);
+  // };
+
+  // const submitHandler = e => {
+  //   e.preventDefault();
+  //   const profile = {
+  //     email: editMail,
+  //     username: editName,
+  //     password: editPassword,
+  //     birth: editBirth,
+  //     phone: editPhone,
+  //   };
+  //   try {
+  //     axios.then(res => console.log(res, profile));
+
+  //   } catch (err) {}
+  // };
+
+  return (
+    <Overlay>
+      <div className="modalContainer">
+        <button onClick={onClose} className="closeBtn" aria-label="Close">
+          X
+        </button>
+        <div className="h2">Edit your profile</div>
+        <div className="content">
+          <p>User Name</p>
+          <TextArea
+            border={'1px solid var(--black-006)'}
+            width={'100%'}
+            height={'100%'}
+            margin={'15px 0 30px 0'}
+            borderRadius={'3px'}
+            padding={'10px 15px 0 15px'}
+            fontSize={'var(--font-size-md)'}
+            fontColor={'var(--black-002)'}
+            placeholder={'Codestates'}
+          />
+          <p>Phone Number</p>
+          <TextArea
+            border={'1px solid var(--black-006)'}
+            width={'100%'}
+            height={'100%'}
+            margin={'15px 0 30px 0'}
+            borderRadius={'3px'}
+            padding={'10px 15px 0 15px'}
+            fontSize={'var(--font-size-md)'}
+            fontColor={'var(--black-002)'}
+            placeholder={'010-1234-5678'}
+            onChange={handlePhoneNumChange}
+          />
+          <p>E-mail</p>
+          <TextArea
+            border={'1px solid var(--black-006)'}
+            width={'100%'}
+            height={'100%'}
+            margin={'15px 0 30px 0'}
+            borderRadius={'3px'}
+            padding={'10px 15px 0 15px'}
+            fontSize={'var(--font-size-md)'}
+            fontColor={'var(--black-002)'}
+            placeholder={'codestates@gmail.com'}
+          />
+          <p>Birth Date</p>
+          <TextArea
+            border={'1px solid var(--black-006)'}
+            width={'100%'}
+            height={'100%'}
+            margin={'15px 0 50px 0'}
+            borderRadius={'3px'}
+            padding={'10px 15px 0 15px'}
+            fontSize={'var(--font-size-md)'}
+            fontColor={'var(--black-002)'}
+            placeholder={'1990.00.00'}
+            onChange={handleBirthDateChange}
+          />
+        </div>
+        <div className="button">
+          <Button
+            text={'Delete Account'}
+            textColor={'#fff'}
+            bgColor={'var(--black-006)'}
+            width={'6vw'}
+            height={'40px'}
+            fontSize={'var(--font-size-md)'}
+            hover={'var(--black-004)'}
+            active={'var(--black-003)'}
+          />
+          <Button
+            text={'Submit Profile'}
+            textColor={'#fff'}
+            bgColor={'var(--main-002)'}
+            width={'6vw'}
+            height={'40px'}
+            fontSize={'var(--font-size-md)'}
+            hover={'#f0820e'}
+            active={'#d1710a'}
+          />
+        </div>
+      </div>
+    </Overlay>
+  );
+};
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.4);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+
+  & > .modalContainer {
+    max-width: 600px;
+    width: 100%;
+    position: fixed;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 50px 40px 50px 40px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    background-color: #ffffff;
+    box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.75);
+  }
+  & .closeBtn {
+    position: fixed;
+    top: 35px;
+    right: 30px;
+    cursor: pointer;
+    color: var(--black-004);
+    font-weight: bold;
+    font-size: var(--font-size-h3);
+    background-color: white;
+  }
+  & .h2 {
+    font-size: var(--font-size-h2);
+    font-weight: bold;
+    padding-bottom: 30px;
+    border-bottom: 1px solid var(--line-001);
+  }
+  & .content {
+    margin-top: 40px;
+    & > p {
+      font-size: var(--font-size-h5);
+      font-weight: bold;
+    }
+  }
+  & TextArea {
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    ::placeholder {
+      color: var(--black-006);
+    }
+  }
+  & .button {
+    display: flex;
+    justify-content: center;
+    & > Button {
+      margin: 0 15px 0 15px;
+    }
+  }
+`;
+
+export default EditModal;
---
+++ src/pages/MyPage/MyPage.js
@@ -0,0 +1,319 @@
+import Pagenation from '../../components/Pagenation';
+import questions from '../../data/message_question.json';
+import answers from '../../data/message_answer.json';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import MainLayout from '../../components/MainLayout';
+import styled from 'styled-components';
+import Button from '../../components/Button';
+import myImg from '../../assets/myImg.svg';
+import phonenum from '../../assets/phonenum.svg';
+import mail from '../../assets/mail.svg';
+import birthday from '../../assets/birthday.svg';
+import EditModal from '../MyPage/EditModal';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+function MyPage() {
+  const [questionsData, setQuestionsData] = useState([]);
+  const [answersData, setAnswersData] = useState([]);
+  const [questionLimit, setQuestionLimit] = useState(5);
+  const [questionPage, setQuestionPage] = useState(1);
+  const [answerLimit, setAnswerLimit] = useState(5);
+  const [answerPage, setAnswerPage] = useState(1);
+  const questionOffset = (questionPage - 1) * questionLimit;
+  const answerOffset = (answerPage - 1) * answerLimit;
+  const questionTotal = questionsData.length;
+  const answerTotal = answersData.length;
+  const [openModal, setOpenModal] = useState(false);
+  // const [userData, setUserData] = useState([]);
+  const [username, setUserName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [birth, setBirth] = useState('');
+  const { user } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    fetch(
+      'http://ec2-43-201-82-36.ap-northeast-2.compute.amazonaws.com:8080/questions',
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setQuestionsData(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(
+      'http://ec2-43-201-82-36.ap-northeast-2.compute.amazonaws.com:8080/questions',
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setAnswersData(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    setQuestionsData(questions.questions);
+  }, []);
+
+  useEffect(() => {
+    setAnswersData(answers.answers);
+  }, []);
+
+  useEffect(() => {
+    if (user && user.memberid && user.authorization) {
+      axios
+        .get(`/api/members/${user.memberid}`, {
+          headers: {
+            Authorization: user.authorization,
+          },
+        })
+        .then(response => {
+          setUserName(response.data.data.username);
+          setPhone(response.data.data.phone);
+          setEmail(response.data.data.email);
+          setBirth(response.data.data.birth);
+          // setUserData(response.data.data);
+        })
+        .catch(err => console.log(err));
+    }
+  }, [user]);
+
+  return (
+    <>
+      <EditModal open={openModal} onClose={() => setOpenModal(false)} />
+      <MainLayout sideBar>
+        <UserInfoContainer>
+          <img src={myImg} alt="myImg" />
+          <UserInfoWrapper>
+            <h2>{username}</h2>
+            <div>
+              <img src={phonenum} alt="phone" />
+              <span>{phone}</span>
+              <img src={mail} alt="e-mail" />
+              <span>{email}</span>
+              <img src={birthday} alt="birthday" />
+              <span>{birth}</span>
+            </div>
+          </UserInfoWrapper>
+          <Button
+            onClick={() => setOpenModal(true)}
+            text={'Edit Profile'}
+            textColor={'#fff'}
+            bgColor={'var(--main-002)'}
+            width={'4vw'}
+            height={'35px'}
+            fontSize={'var(--font-size-md)'}
+            hover={'#f0820e'}
+            active={'#d1710a'}
+          />
+        </UserInfoContainer>
+        <QuestionContainer>
+          <h2>Question</h2>
+          <QuestionInfo>
+            {questionsData
+              .slice(questionOffset, questionOffset + questionLimit)
+              .map(({ questionId, title, createdAt }, index) => {
+                const postNumber = questionOffset + index + 1;
+                return (
+                  <li key={questionId}>
+                    <div className="qcontainer">
+                      <span className="qnumber">
+                        {postNumber.toString().padStart(2, '0')}
+                      </span>
+                      <Link
+                        className="qlist"
+                        to={`/board-detail/${questionId}`}
+                      >
+                        {title}
+                      </Link>
+                      <span className="qdate"> {`${createdAt}`}</span>
+                    </div>
+                  </li>
+                );
+              })}
+          </QuestionInfo>
+        </QuestionContainer>
+        <Pagenation
+          limit={questionLimit}
+          setLimit={setQuestionLimit}
+          page={questionPage}
+          setPage={setQuestionPage}
+          total={questionTotal}
+        />
+        <AnswerContainer>
+          <h2>Answer</h2>
+          <AnswerInfo>
+            {answersData
+              .slice(answerOffset, answerOffset + answerLimit)
+              .map(({ questionId, body, createdAt }, index) => {
+                const postNumber = answerOffset + index + 1;
+                return (
+                  <li key={questionId}>
+                    <div className="acontainer">
+                      <span className="anumber">
+                        {postNumber.toString().padStart(2, '0')}
+                      </span>
+                      <Link
+                        className="alist"
+                        to={`/board-detail/${questionId}`}
+                      >
+                        {body}
+                      </Link>
+                      <span className="adate"> {`${createdAt}`}</span>
+                    </div>
+                  </li>
+                );
+              })}
+          </AnswerInfo>
+        </AnswerContainer>
+        <Pagenation
+          limit={answerLimit}
+          setLimit={setAnswerLimit}
+          page={answerPage}
+          setPage={setAnswerPage}
+          total={answerTotal}
+        />
+      </MainLayout>
+    </>
+  );
+}
+
+const UserInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid var(--line-002);
+  margin-top: 15px;
+  padding-bottom: 30px;
+  color: var(--black-001);
+  & > :nth-child(2) {
+    flex-grow: 1;
+  }
+  & > :last-child {
+    margin-top: 35px;
+  }
+  & h2 {
+    font-size: var(--font-size-h2);
+    font-weight: bold;
+    margin-bottom: 18px;
+  }
+  & span {
+    font-size: var(--font-size-md);
+    font-weight: bold;
+    color: var(--black-004);
+  }
+`;
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 35px;
+  & > div {
+    display: flex;
+    & > :nth-child(odd) {
+      margin-right: 6px;
+    }
+    & > :nth-child(even) {
+      margin-right: 30px;
+    }
+  }
+`;
+
+const QuestionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: var(--font-size-md);
+  color: var(--black-001);
+  width: 900px;
+  margin-top: 35px;
+  margin-bottom: 20px;
+  & > h2 {
+    font-size: var(--font-size-h4);
+    font-weight: bold;
+  }
+`;
+
+const QuestionInfo = styled.ul`
+  display: flex;
+  flex-direction: column;
+  font-size: var(--font-size-lg);
+  padding: 25px;
+  margin-top: 20px;
+  width: 950px;
+  border: 1px solid var(--line-001);
+  border-radius: 5px;
+  & .qcontainer {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+  }
+  & .qnumber {
+    color: var(--main-002);
+    padding-right: 15px;
+  }
+  & .qlist {
+    width: 600px;
+    flex-grow: 1;
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  & .qdate {
+    color: var(--black-006);
+    margin-left: 60px;
+  }
+`;
+
+const AnswerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: var(--font-size-md);
+  color: var(--black-001);
+  margin-top: 35px;
+  margin-bottom: 20px;
+  & > h2 {
+    font-size: var(--font-size-h4);
+    font-weight: bold;
+  }
+`;
+
+const AnswerInfo = styled.ul`
+  display: flex;
+  flex-direction: column;
+  font-size: var(--font-size-lg);
+  padding: 25px;
+  margin-top: 20px;
+  width: 950px;
+  border: 1px solid var(--line-001);
+  border-radius: 5px;
+  & .acontainer {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+  }
+  & .anumber {
+    color: var(--main-002);
+    padding-right: 15px;
+  }
+  & .alist {
+    width: 600px;
+    flex-grow: 1;
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  & .adate {
+    color: var(--black-006);
+    margin-left: 60px;
+  }
+`;
+
+export default MyPage;
